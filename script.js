19af0f7f28aeaa82e6732b1d00187084db8f77e2
Jeffrey Lee <jeffylee87@hotmail.com>
2018-06-04 20:02:40 -0700

    update

---
+++ script.js
@@ -0,0 +1,27 @@
+function changeToSecondPage() {
+    window.location.href = "secondpage.html";
+};
+
+$(".fa-lock").draggable({
+    revert: true,
+    start: function(event, ui) {
+      $(".bubble").addClass("expand");
+    },
+    stop: function(event, ui) {
+  
+      if (ui.position.left < 30 || ui.position.left > 270 || ui.position.top < 30 || ui.position.top > 270) {
+  
+        // Unlock
+        $(".fa-check").addClass("return");
+        $(".fa-lock").addClass("away");
+        $(".lock").addClass("good");
+      }
+  
+      $(".bubble").removeClass("expand");
+      
+      changeToSecondPage();
+    }
+    
+  });
+
+  
